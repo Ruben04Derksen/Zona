1996faf182526930a5442654cdc0da000858bc48
crlse42 <carlosdlt42@hotmail.com>
2023-07-31 19:24:15 -0700

    Getting new API for Project

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,5 @@
-const url = 'https://streaming-availability.p.rapidapi.com/v2/services';
-const options = {
+var url = 'https://streaming-availability.p.rapidapi.com/v2/search/title?title=batman&country=us&show_type=movie&output_language=en';
+var options = {
 	method: 'GET',
 	headers: {
 		'X-RapidAPI-Key': 'bbf8c25035msh1581a5baf939adap1808b3jsnde0cea465c6b',
@@ -8,10 +8,12 @@ const options = {
 };
 
 try {
-	const response = fetch(url, options)
+	var response = fetch(url, options)
     .then(response => response.json())
     .then(data => console.log(data))
 	
 } catch (error) {
 	console.error(error);
 }
+
+
---
+++ assets/script1.js
@@ -0,0 +1,19 @@
+url = 'https://moviesdatabase.p.rapidapi.com/titles/random?list=most_pop_movies';
+var settings = {
+	async: true,
+	crossDomain: true,
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'bbf8c25035msh1581a5baf939adap1808b3jsnde0cea465c6b',
+		'X-RapidAPI-Host': 'moviesdatabase.p.rapidapi.com'
+	}
+};
+
+try {
+	response = fetch(url, settings)
+    .then(response => response.json())
+    .then(data => console.log(data))
+
+} catch (error) {
+	console.error(error);
+}
